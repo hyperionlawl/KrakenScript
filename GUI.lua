c01b33f3cdd3891de087dafa8217688a3388f435
hyperionlawl <167688609+hyperionlawl@users.noreply.github.com>
2024-05-04 15:56:32 +0100

    Update and rename src.lua to GUI.lua

---
+++ GUI.lua
@@ -0,0 +1,6 @@
+game.StarterGui:SetCore("SendNotification", {
+		Title = "PrismHub: PrismTeam";
+		Text = "This script is not ready for the public yet!";
+		Icon = "rbxassetid://0"; 
+		Duration = 5;
+	})
