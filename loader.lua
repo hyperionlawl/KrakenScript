6e1f22c6c39cc3b953b622685ee4632d5ac1fbe9
hyperionlawl <167688609+hyperionlawl@users.noreply.github.com>
2024-05-06 12:32:49 +0100

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,1 +1,1 @@
-loadstring(game:HttpGet('https://raw.githubusercontent.com/hyperionlawl/PrismHub/main/GUI.lua'))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/hyperionlawl/PrismHub/main/BetaGUI.lua"))()
