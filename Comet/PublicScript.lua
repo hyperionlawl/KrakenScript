36aa7cf173ed77762586f830fdbd8488db39fe6a
hyperionlawl <167688609+hyperionlawl@users.noreply.github.com>
2024-05-11 22:59:48 +0100

    Update PublicScript.lua

--- Comet/PublicScript.lua
+++ Comet/PublicScript.lua
@@ -26,9 +26,11 @@ local Window = Rayfield:CreateWindow({
    }
 })
 
-local MainPage = Window:CreateTab(". main")
+local MainPage = Window:CreateTab(". quick access")
 local PlayerPage = Window:CreateTab(". player")
+local GamesPage = Window:CreateTab(". games")
 
+-- MAIN TAB
 local ESP = {}
 ESP.main = MainPage:CreateButton({
    Name = "ESP",
@@ -65,6 +67,52 @@ cosmoexec.main = MainPage:CreateButton({
        })
        wait(10)
        plr:Kick("Bye bye ;)")
+   end,
+})
+
+-- PLAYER TAB:
+
+local WalkspeedSlider = PlayerPage:CreateSlider({
+   Name = "Walkspeed",
+   Range = {0, 100},
+   Increment = 10,
+   Suffix = " ",
+   CurrentValue = 10,
+   Flag = "WalkSpeedSlider",
+   Callback = function(Value)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+character.Humanoid.WalkSpeed = value
+   end,
+})
+
+local JumpSlider = PlayerPage:CreateSlider({
+   Name = "Jump",
+   Range = {0, 100},
+   Increment = 10,
+   Suffix = " ",
+   CurrentValue = 10,
+   Flag = "JumpPowerSlider",
+   Callback = function(Value)
+local player = game.Players.LocalPlayer
+local character = player.Character or player.CharacterAdded:Wait()
+character.Humanoid.JumpPower = value
+   end,
+})
+
+-- GAMES TAB: (MOST LINES)
+
+local Button = GamesPage:CreateButton({
+   Name = "Arsenal",
+   Callback = function()
+
+   end,
+})
+
+local Button = GamesPage:CreateButton({
+   Name = "Blox Fruits",
+   Callback = function()
+
    end,
 })
 
